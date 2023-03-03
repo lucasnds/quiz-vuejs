4278a5da1b27ebae4bb96eb266830b6624fd8bd5
Lucas <lucasdoismile16@gmail.com>
2023-03-02 21:18:02 -0300

    falta validar as respostas, placar, event clicks

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,43 @@
 <template>
-  
-  <HelloWorld msg="My first aplication with vue.js"/>
+  <template v-if="this.question">
+    <h1 v-html="this.question"></h1>
+
+    <template v-for="(answer,index) in this.answers" :key="index">
+        <input type="radio" name="options" :value="answer">
+        <label v-html="answer"></label><br>
+    </template>
+    <button class="send" type="button">Confirmar</button>
+  </template> 
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data(){
+    return{
+      question:undefined,
+      incorrectAnswers:undefined,
+      correctAnswer:undefined
+    }
+  },
+  computed:{
+    amswers(){
+      var answers =JSON.parse(JSON.stringify(this.incorrectAnswers)) 
+      answers.splice(Math.round(Math.random()*answers.length),0,this.correctAnswer)
+      return answers
+    }
+  },
+ created(){
+  this.axios.get('https://opentdb.com/api.php?amount=1&category=18').then((response) => {
+    this.question=response.data.results[0].question
+    this.incorrectAnswers=response.data.results[0].incorrect_answers
+    this.correctAnswer=response.data.results[0].correct_answer
+  console.log(response.data.results[0])
+})
+ }
+  //https://opentdb.com/api.php?amount=1&category=18
 }
 </script>
 
@@ -21,6 +48,37 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 60px auto;
+  max-width: 960px;
+}
+h1 {
+  margin-top: 40px;
+}
+
+input[type='radio']{
+  margin: 12px 4px;
+}
+
+button.send {
+  margin-top: 12px;
+  height: 40px;
+  min-width: 120px;
+  padding: 0 16px;
+  color: #fff;
+  background-color: #1867c0;
+  border: 1px solid #1867c0;
+  cursor: pointer;
+}
+
+section.score {
+  border-bottom: 1px solid black;
+  padding: 24px;
+  font-size: 18px;
+
+  span {
+    padding: 8px;
+    font-weight: bold;
+    border: 1px solid black;
+  }
 }
 </style>

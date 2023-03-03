a80b2594354a0d0d89be75c598a6ca6bd1b68735
Lucas <lucasdoismile16@gmail.com>
2023-03-02 23:32:22 -0300

    falta o score

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,29 @@
 <template>
+
+  <section class="score">
+    Jogador <span></span> x 
+   <span></span> Computador
+  </section>
+
   <template v-if="this.question">
     <h1 v-html="this.question"></h1>
 
     <template v-for="(answer,index) in this.answers" :key="index">
-        <input type="radio" name="options" :value="answer">
+        <input type="radio" name="options" :value="answer" v-model="this.chosenAnswer" :disabled="this.answerSubmitted">
         <label v-html="answer"></label><br>
     </template>
-    <button class="send" type="button">Confirmar</button>
+    <button v-if="!this.answerSubmitted" @click="this.submitAnswer()" class="send" type="button">Confirmar</button>
   </template> 
+
+  <section class="result" v-if="this.answerSubmitted">
+    <template v-if="this.chosenAnswer == this.correctAnswer">
+      <h4 v-html="'&#9989; Parabéns, a resposta ' + this.correctAnswer + ' está correta.'"></h4>
+    </template>
+    <template v-else>
+      <h4 v-html="'&#10060;  Que pena, a resposta está errada. A resposta correta é ' + this.correctAnswer + '.'"></h4>
+    </template>
+    <button @click="this.getNewQuestion()" class="send" type="button">Próxima pergunta</button>
+  </section>
 </template>
 
 <script>
@@ -19,25 +35,53 @@ export default {
     return{
       question:undefined,
       incorrectAnswers:undefined,
-      correctAnswer:undefined
+      correctAnswer:undefined,
+      chosenAnswer:undefined,
+      answerSubmitted:false
     }
   },
+  methods:{
+    submitAnswer(){
+      if(!this.chosenAnswer){// or this.chosenAnswer==undefined
+        console.log('Pick one of the options')
+      }else{
+        this.answerSubmitted=true
+        if(this.chosenAnswer==this.correctAnswer){
+          console.log('You got it right')
+         
+        }else{
+          console.log('You got it wrong')
+         
+        }
+      }
+    },
+    getNewQuestion(){
+this.answerSubmitted=false
+this.chosenAnswer=undefined
+this.question= undefined
+      
+      this.axios.get('https://opentdb.com/api.php?amount=1&category=18').then((response) => {
+    this.question=response.data.results[0].question
+    this.incorrectAnswers=response.data.results[0].incorrect_answers
+    this.correctAnswer=response.data.results[0].correct_answer
+  console.log(response.data.results[0])
+})
+    }
+   
+
+  },
   computed:{
-    amswers(){
+    answers(){
       var answers =JSON.parse(JSON.stringify(this.incorrectAnswers)) 
       answers.splice(Math.round(Math.random()*answers.length),0,this.correctAnswer)
       return answers
     }
   },
  created(){
-  this.axios.get('https://opentdb.com/api.php?amount=1&category=18').then((response) => {
-    this.question=response.data.results[0].question
-    this.incorrectAnswers=response.data.results[0].incorrect_answers
-    this.correctAnswer=response.data.results[0].correct_answer
-  console.log(response.data.results[0])
-})
+  this.getNewQuestion()
+ 
  }
-  //https://opentdb.com/api.php?amount=1&category=18
+
 }
 </script>
 
